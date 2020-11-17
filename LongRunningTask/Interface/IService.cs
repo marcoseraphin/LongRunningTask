f1163ca9074ed6a15aecc1d9962e49e0d1d72e69
Marco Seraphin <seraphin@develope-it.de>
2020-11-17 14:51:33 +0100

    - added background service in info.plist

--- LongRunningTask/Interface/IService.cs
+++ LongRunningTask/Interface/IService.cs
@@ -3,6 +3,9 @@ using System.Threading.Tasks;
 
 namespace LongRunningTask.Interface
 {
+	/// <summary>
+	/// Sample Counter service
+	/// </summary>
 	public interface IService
 	{
 		Task<int> Add();
